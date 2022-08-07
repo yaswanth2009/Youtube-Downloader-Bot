f3c4ca466f5debc012bcf7849f1f5d59333f546b
yaswanth2009 <108663991+yaswanth2009@users.noreply.github.com>
2022-08-07 08:44:40 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -9,6 +9,6 @@ async def start(client, message):
         [InlineKeyboardButton(
             "Report Bugs 😊", url="https://t.me/dl_bots_supports")]
     ])
-    welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
+    welcomed = f"Hey <b>{message.from_user.first_name Welcome to YouTupe Downloader Bot}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
     raise StopPropagation
